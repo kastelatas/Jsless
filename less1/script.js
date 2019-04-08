d26c66b7a72f19e7ef6184cad67202ca56adf522
kasteltas <darckglad@gmail.com>
2019-04-08 16:01:54 +0300

    Это наш первый комит

---
+++ less1/script.js
@@ -0,0 +1,30 @@
+// var obj = {};
+
+// let persone = {
+//     name: "Jone",
+//     age: 25,
+//     isMarried: false
+// }
+
+// console.log(persone["name"]);
+
+// let arr = ['plum.png','opange.jpg','apple.bmp'];
+
+// console.log(arr[0])
+
+// let answer = confirm(" Are you here?");
+
+// console.log(answer);
+
+// let answer = prompt("Есть ли вам 18?", "Да");
+
+// console.log(answer);
+
+let incr = 10,
+    decr = 10;
+
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr);
---
+++ less1/tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr);
